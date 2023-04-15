1b725e5fe0059b8202ec4cf62a9807d7e8f180a1
katfogy <katfogy@gmail.com>
2023-04-15 13:01:42 +0100

    speakers store as array in js app.js

--- app.js
+++ app.js
@@ -1,44 +1,44 @@
-const speakers=[
-    {
-        id:1,
-        url: "asset/speakers/aliko-dangote.png",
-        name: "Aliko Dangote",
-        job:"Nigerian business magnate",
-        desc: "Aliko Dangote GCON is a Nigerian business magnate who is the founder, chairman, and CEO of the Dangote Group, the largest industrial conglomerate in West Africa."
-    },
-    {
-        id:2,
-        url: "asset/speakers/femi-otedola.png",
-        name: "Femi Otedola",
-        job:"Nigerian businessman",
-        desc: "Femi Otedola is a Nigerian businessman and philanthropist. He is the former chairman of Forte Oil PLC, and is the current executive chairman of Geregu Power PLC."
-    },
-    {
-        id:3,
-        url: "asset/speakers/mike-adenuga.png",
-        name: "Mike Adenuga",
-        job:"Nigerian businessman",
-        desc: "Michael Adeniyi Agbolade Ishola Adenuga Jr CSG GCON is a Nigerian billionaire businessman, and the sixth richest person in Africa. His company Globacom is Nigeria's second-largest telecom operator"
-    },
-    {
-        id:4,
-        url: "asset/speakers/peter-obi.png",
-        name: "Peter Gregory Obi",
-        job:"Former Governor of Anambra State",
-        desc: "Peter Gregory Obi CON is a Nigerian businessman and politician who served as governor of Anambra from March to November 2006, February to May 2007, and June 2007 to March 2014."
-    },
-    {
-        id:5,
-        url: "asset/speakers/tony-elumelu.png",
-        name: "Tony O. Elumelu",
-        job:"Nigerian economist",
-        desc: "Anthony Onyemaechi Elumelu CFR is a Nigerian economist, and philanthropist. He chairs Heirs Holdings, Transcorp and is the founder of The Tony Elumelu Foundation."
-    },
-    {
-        id:6,
-        url: "asset/speakers/yemi-osinbajo.png",
-        name: "Yemi Osinbajo",
-        job:"Vice President of Nigeria",
-        desc: "Oluyemi Oluleke Osinbajo GCON is a Nigerian lawyer, professor, and politician who is the 14th and current vice president of Nigeria since 2015."
-    }
-]
+const speakers = [
+  {
+    id: 1,
+    url: 'asset/speakers/aliko-dangote.png',
+    name: 'Aliko Dangote',
+    job: 'Nigerian business magnate',
+    desc: 'Aliko Dangote GCON is a Nigerian business magnate who is the founder, chairman, and CEO of the Dangote Group, the largest industrial conglomerate in West Africa.',
+  },
+  {
+    id: 2,
+    url: 'asset/speakers/femi-otedola.png',
+    name: 'Femi Otedola',
+    job: 'Nigerian businessman',
+    desc: 'Femi Otedola is a Nigerian businessman and philanthropist. He is the former chairman of Forte Oil PLC, and is the current executive chairman of Geregu Power PLC.',
+  },
+  {
+    id: 3,
+    url: 'asset/speakers/mike-adenuga.png',
+    name: 'Mike Adenuga',
+    job: 'Nigerian businessman',
+    desc: "Michael Adeniyi Agbolade Ishola Adenuga Jr CSG GCON is a Nigerian billionaire businessman, and the sixth richest person in Africa. His company Globacom is Nigeria's second-largest telecom operator",
+  },
+  {
+    id: 4,
+    url: 'asset/speakers/peter-obi.png',
+    name: 'Peter Gregory Obi',
+    job: 'Former Governor of Anambra State',
+    desc: 'Peter Gregory Obi CON is a Nigerian businessman and politician who served as governor of Anambra from March to November 2006, February to May 2007, and June 2007 to March 2014.',
+  },
+  {
+    id: 5,
+    url: 'asset/speakers/tony-elumelu.png',
+    name: 'Tony O. Elumelu',
+    job: 'Nigerian economist',
+    desc: 'Anthony Onyemaechi Elumelu CFR is a Nigerian economist, and philanthropist. He chairs Heirs Holdings, Transcorp and is the founder of The Tony Elumelu Foundation.',
+  },
+  {
+    id: 6,
+    url: 'asset/speakers/yemi-osinbajo.png',
+    name: 'Yemi Osinbajo',
+    job: 'Vice President of Nigeria',
+    desc: 'Oluyemi Oluleke Osinbajo GCON is a Nigerian lawyer, professor, and politician who is the 14th and current vice president of Nigeria since 2015.',
+  },
+];
